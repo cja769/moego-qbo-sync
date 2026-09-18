bf08daaee1a00a0162fe6064822be08c8a6fc362
Jay Aisenbrey <caisenbrey@obm.mn>
2026-09-18 13:15:34 -0500

    Create MoeGoSyncApplication.java

---
+++ src/main/java/io/github/thirdcoast/moego/MoeGoSyncApplication.java
@@ -0,0 +1,9 @@
+package io.github.thirdcoast.moego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeGoSyncApplication {
+    public static void main(String[] args) { SpringApplication.run(MoeGoSyncApplication.class, args); }
+}
